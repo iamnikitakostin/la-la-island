336e1d8165e8dbf9fccf3c0af86243172e49bc7c
iamnikitakostin <contact@nikitakostin.com>
2025-01-27 19:38:33 -0800

    new opt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,23 +6,34 @@ function Hero() {
   return (
     <div className="flex flex-col items-start text-center md:text-left md:w-1/2 z-10 px-6">
       <div className="max-w-[400px] mx-auto aspect-square">
-        <img
-          srcSet={`${logoLg} 500w,
-                  ${logoSm} 300w`}
-          sizes="(max-width: 768px) 300px, 500px"
-          src={logoLg}
-          alt="app logo"
-          className="w-full"
-        />
+        <picture>
+          <source
+            srcSet={logoLg}
+            media="(min-width: 768px)"
+            type="image/webp"
+          />
+          <source
+            srcSet={logoSm}
+            media="(max-width: 767px)"
+            type="image/webp"
+          />
+          <img
+            src={logoLg} 
+            alt="app logo"
+            className="w-full"
+            width="500"
+            height="500" 
+          />
+        </picture>
       </div>
-      <p className="mt-6 text-xl md:text-2xl font-medium text-center md:text-left text-amber-50 
+      <p
+        className="mt-6 text-xl md:text-2xl font-medium text-center md:text-left text-amber-50 
            leading-relaxed tracking-wide bg-black/40 px-6 py-3 rounded-lg
-           shadow-xl backdrop-blur-sm">
+           shadow-xl backdrop-blur-sm"
+      >
         A cup of coffee & a croissant,<br />
         <span className="text-amber-200 font-semibold">What could be better?</span>
       </p>
-
-
     </div>
   );
 }
