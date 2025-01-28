715000ad7cb45e2c064ea21565629971225d2782
iamnikitakostin <contact@nikitakostin.com>
2025-01-27 20:36:11 -0800

    new opt

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+  plugins: [
+    react(),
+  ],
+  build: {
+    assetsDir: 'assets',
+    target: 'esnext', // Use modern JavaScript features
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Code-splitting for vendor files
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     },
-  }
-})
+    minify: 'terser', // Minify JavaScript
+    assetsInlineLimit: 4096, // Inline small assets to reduce HTTP requests
+  },
+  server: {
+    open: true, // Automatically open the app in the browser
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'], // Pre-bundling important dependencies
+  },
+});
